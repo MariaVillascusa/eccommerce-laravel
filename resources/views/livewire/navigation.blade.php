5097cd91d940d4baa4b9e54a77fd1c95d69ad54f
Maria Villascusa <meri.villa14@gmail.com>
2022-01-06 01:55:25 +0100

    menu functionality

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,23 +1,13 @@
-<style>
-    #navigation-menu {
-        height: calc(100vh - 4rem);
-    }
-
-    .navigation-link:hover .navigation-submenu{
-        display:block !important;
-    }
-
-</style>
-
-<header class="bg-trueGray-700 sticky top-0">
+<header class="bg-trueGray-700 sticky top-0" x-data="dropdown()">
     <div class="container flex items-center h-16">
-        <a
+        <a :class="{'bg-opacity-100 text-orange-500':open}"
+        x-on:click="show()"
             class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M4 6h16M4 12h16M4 18h16" />
             </svg>
-            <span>
+            <span class="text-sm">
                 Categorías
             </span>
         </a>
@@ -55,7 +45,7 @@
                             @csrf
 
                             <x-jet-dropdown-link href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
+                                                                            this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-jet-dropdown-link>
                         </form>
@@ -83,9 +73,10 @@
         @livewire('dropdown-cart')
     </div>
 
-    <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
+    <nav id="navigation-menu" x-show="open" :class="{'block':open, 'hidden':!open}"
+        class="bg-trueGray-700 bg-opacity-25 w-full absolute hidden">
         <div class="container h-full">
-            <div class="grid grid-cols-4 h-full relative">
+            <div x-on:click.away="close()" class="grid grid-cols-4 h-full relative">
                 <ul class="bg-white">
                     @foreach ($categories as $category)
                         <li class="navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
