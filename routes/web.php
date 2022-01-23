cb1f25bd74ad2fde6f9660be7cebc758e97d31d9
Maria Villascusa <meri.villa14@gmail.com>
2022-01-23 19:50:11 +0100

    showing search results

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductsController;
@@ -21,6 +22,8 @@ Route::get('/', WelcomeController::class);
 Route::get('categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 
 Route::get('products/{product}', [ProductsController::class, 'show'])->name('products.show');
+
+Route::get('search', SearchController::class)->name('search');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
