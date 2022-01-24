f6f300f7ccb0c5452ab80cc9146b222725b35de0
Maria Villascusa <meri.villa14@gmail.com>
2022-01-24 13:23:30 +0100

    shopping cart funcionality and final design

--- app/Http/Livewire/ShoppingCart.php
+++ app/Http/Livewire/ShoppingCart.php
@@ -3,9 +3,24 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class ShoppingCart extends Component
 {
+    public $listeners = ['render'];
+
+    public function destroy()
+    {
+        Cart::destroy();
+        $this->emitTo('dropdown-cart', 'render');
+    }
+
+    public function delete($rowId)
+    {
+        Cart::remove($rowId);
+        $this->emit('render');
+    }
+
     public function render()
     {
         return view('livewire.shopping-cart');
