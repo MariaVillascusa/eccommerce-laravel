a77a59444a90fcc976390f4ffa22f1fb7a1dc019
Maria Villascusa <meri.villa14@gmail.com>
2022-01-24 16:20:52 +0100

    events & listeners - login/logout

---
+++ app/Listeners/MergeTheCartLogout.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Auth\Events\Logout;
+
+class MergeTheCartLogout
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\Logout  $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+        Cart::erase(auth()->user()->id);
+        Cart::store(auth()->user()->id);
+    }
+}
