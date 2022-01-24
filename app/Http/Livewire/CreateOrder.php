cced0f2de375152425aaa1f1a7355d13492ca493
Maria Villascusa <meri.villa14@gmail.com>
2022-01-25 00:47:02 +0100

    creating order - adress form

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -3,9 +3,20 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Department;
 
 class CreateOrder extends Component
 {
+    public $departments, $cities = [], $districts = [];
+    public $department_id = '', $city_id = '', $district_id = '';
+    public $address, $reference;
+    public $envio_type = 1;
+
+    public function mount()
+    {
+        $this->departments = Department::all();
+    }
+
     public function render()
     {
         return view('livewire.create-order');
