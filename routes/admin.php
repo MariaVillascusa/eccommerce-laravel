5ee4806121788bf72d2650878f7ff6019f9ea3c9
Maria Villascusa <meri.villa14@gmail.com>
2022-01-28 19:07:43 +0100

    products filter funcionality and pagination + create button

--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\ShowProducts;
 
 Route::get('/', ShowProducts::class)->name('admin.index') ;
+
+Route::get('products/create', function () {})->name('admin.products.create');
 Route::get('products/{product}/edit', function () {})->name('admin.products.edit');
