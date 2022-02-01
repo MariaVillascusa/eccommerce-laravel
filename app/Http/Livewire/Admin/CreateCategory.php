fb08790fc98d3b3b68e564e795587c4bb5058039
Maria Villascusa <meri.villa14@gmail.com>
2022-02-01 17:06:45 +0100

    create category form

---
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\Brand;
+use Livewire\Component;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Livewire\WithFileUploads;
+
+class CreateCategory extends Component
+{
+    use WithFileUploads;
+
+    public $brands, $image;
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'icon' => null,
+        'image' => null,
+        'brands' => [],
+    ];
+
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:categories,slug',
+        'createForm.icon' => 'required',
+        'createForm.image' => 'required|image|max:1024',
+        'createForm.brands' => 'required',
+    ];
+
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.icon' => 'icono',
+        'createForm.image' => 'imagen',
+        'createForm.brands' => 'marcas',
+    ];
+
+    public function mount()
+    {
+        $this->getBrands();
+        $this->image = 1;
+    }
+
+    public function getBrands()
+    {
+        $this->brands = Brand::all();
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $image = $this->createForm['image']->store('categories', 'public');
+        $category = Category::create([
+            'name' => $this->createForm['name'],
+            'slug' => $this->createForm['slug'],
+            'icon' => $this->createForm['icon'],
+            'image' => $image
+        ]);
+        $category->brands()->attach($this->createForm['brands']);
+        $this->image = 2;
+        $this->reset('createForm');
+    }
+
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.create-category');
+    }
+}
