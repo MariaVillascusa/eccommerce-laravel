066882fd1f33de536b5197cdbbb2aae9caa2e802
Maria Villascusa <meri.villa14@gmail.com>
2022-02-02 17:56:52 +0100

    categories names as links

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\EditProduct;
+use App\Http\Livewire\Admin\ShowCategory;
 use App\Http\Livewire\Admin\ShowProducts;
 use App\Http\Livewire\Admin\CreateProduct;
 use App\Http\Controllers\Admin\ProductController;
@@ -11,6 +12,8 @@ Route::get('/', ShowProducts::class)->name('admin.index') ;
 
 Route::get('products/create', CreateProduct::class)->name('admin.products.create');
 Route::get('products/{product}/edit', EditProduct::class)->name('admin.products.edit');
+
 Route::get('categories', [CategoryController::class, 'index'])->name('admin.categories.index');
+Route::get('categories/{category}', ShowCategory::class)->name('admin.categories.show');
 
 Route::post('product/{product}/files', [ProductController::class, 'files'])->name('admin.products.files');
