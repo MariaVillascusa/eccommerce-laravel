b14c4df35d2a22ff470a649cb8206def55a47162
Maria Villascusa <meri.villa14@gmail.com>
2022-02-05 14:09:17 +0100

    fix: change views to responsive

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,15 +1,20 @@
 <x-app-layout>
     <div class="container py-8">
-        <div class="grid grid-cols-2 gap-6">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
             <div>
                 <div class="flexslider">
                     <ul class="slides">
                         @foreach ($product->images as $image)
                             <li data-thumb="{{ Storage::url($image->url) }}">
-                                <img dusk="image-product-{{ $loop->index }}" src="{{ Storage::url($image->url) }}" />
+                                <img dusk="image-product-{{ $loop->index }}"
+                                    src="{{ Storage::url($image->url) }}" />
                             </li>
                         @endforeach
                     </ul>
+                </div>
+                <div class="-mt-10 text-gray-700">
+                    <h2 class="font-bold text-lg">Descripción</h2>
+                    {!! $product->description !!}
                 </div>
             </div>
             <div>
@@ -27,9 +32,10 @@
                     <div class="flex items-center p-4">
                         <span class="flex items-center justify-center h-10 w-10 rounded-full bg-lime-600">
                             <i class="fas fa-truck text-sm text-white"></i>
-                            </span>
+                        </span>
                         <div class="ml-4">
-                            <p class="text-lg font-semibold text-lime-600">Se hacen envíos solo a la península</p>                             <p>Recíbelo el {{ Date::now()->addDay(7)->locale('es')->format('l j F') }}</p>
+                            <p class="text-lg font-semibold text-lime-600">Se hacen envíos solo a la península</p>
+                            <p>Recíbelo el {{ Date::now()->addDay(7)->locale('es')->format('l j F') }}</p>
                         </div>
                     </div>
                 </div>
